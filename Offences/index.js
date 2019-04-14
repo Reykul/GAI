004acdf2154d18fa371d401ad483fe12630b1887
Stanislav Zagnetov <Stanislav.zagnetov@9yards.at>
2019-04-15 00:44:19 +0300

    implement connection pool for offences

--- Offences/index.js
+++ Offences/index.js
@@ -11,17 +11,12 @@ const mysql = require('mysql');
 const app = express();
 
 //Create Connection
-const conn = mysql.createConnection({
+const connPool = mysql.createPool({
+  connectionLimit: 100,
   host: 'localhost',
   user: 'root',
   password: 'root',
   database: 'offences'
-});
-
-//connect to database
-conn.connect((err) => {
-  if (err) throw err;
-  console.log('Mysql Connected...');
 });
 
 //set views file
@@ -35,52 +30,80 @@ app.use('/assets', express.static(__dirname + '/public'));
 
 //route for homepage
 app.get('/', (req, res) => {
-  let sql = "SELECT * FROM offence";
-  let query = conn.query(sql, (err, results) => {
-    if (err) throw err;
-    res.render('offence_view', {
-      results: results
+  connPool.getConnection((err, conn) => {
+    if (err) {
+      conn.release();
+      return;
+    }
+    let sql = "SELECT * FROM offence";
+    let query = conn.query(sql, (err, results) => {
+      if (err) throw err;
+      res.render('offence_view', {
+        results: results
+      });
     });
+    conn.release();
   });
 });
 
 //route for insert data
 app.post('/save', (req, res) => {
-  let data = {
-    driver_id: req.body.driver_id,
-    offence_description: req.body.offence_description,
-    offence_cost: req.body.offence_cost,
-    deprived_of_license: req.body.deprived_of_license,
-  };
-  let sql = "INSERT INTO offence SET ?";
-  let query = conn.query(sql, data, (err, results) => {
-    if (err) throw err;
-    res.redirect('/');
+  connPool.getConnection((err, conn) => {
+    if (err) {
+      conn.release();
+      return;
+    }
+    let data = {
+      driver_id: req.body.driver_id,
+      offence_description: req.body.offence_description,
+      offence_cost: req.body.offence_cost,
+      deprived_of_license: req.body.deprived_of_license,
+    };
+    let sql = "INSERT INTO offence SET ?";
+    let query = conn.query(sql, data, (err, results) => {
+      if (err) throw err;
+      res.redirect('/');
+    });
+    conn.release();
   });
 });
 
 //route for update data
 app.post('/update', (req, res) => {
-  let sql = "UPDATE offence SET driver_id='" + req.body.driver_id
-    + "', offence_description='" + req.body.offence_description
-    + "', offence_cost='" + req.body.offence_cost
-    + "', deprived_of_license='" + req.body.deprived_of_license
-    + "'  WHERE offence_id=" + req.body.id;
-  let query = conn.query(sql, (err, results) => {
+  connPool.getConnection((err, conn) => {
     if (err) {
-      console.log('ERROR');
-      throw err;
+      conn.release();
+      return;
     }
-    res.redirect('/');
+    let sql = "UPDATE offence SET driver_id='" + req.body.driver_id
+      + "', offence_description='" + req.body.offence_description
+      + "', offence_cost='" + req.body.offence_cost
+      + "', deprived_of_license='" + req.body.deprived_of_license
+      + "'  WHERE offence_id=" + req.body.id;
+    let query = conn.query(sql, (err, results) => {
+      if (err) {
+        console.log('ERROR');
+        throw err;
+      }
+      res.redirect('/');
+    });
+    conn.release();
   });
 });
 
 //route for delete data
 app.post('/delete', (req, res) => {
-  let sql = "DELETE FROM offence WHERE offence_id=" + req.body.offence_id + "";
-  let query = conn.query(sql, (err, results) => {
-    if (err) throw err;
-    res.redirect('/');
+  connPool.getConnection((err, conn) => {
+    if (err) {
+      conn.release();
+      return;
+    }
+    let sql = "DELETE FROM offence WHERE offence_id=" + req.body.offence_id + "";
+    let query = conn.query(sql, (err, results) => {
+      if (err) throw err;
+      res.redirect('/');
+    });
+    conn.release();
   });
 });
 
