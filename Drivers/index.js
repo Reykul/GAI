2e7df13ffbd403f4eb6e0421520feeee021b0f0a
Stanislav Zagnetov <Stanislav.zagnetov@9yards.at>
2019-04-15 00:01:56 +0300

    implement connection pool for drivers

--- Drivers/index.js
+++ Drivers/index.js
@@ -11,17 +11,12 @@ const mysql = require('mysql');
 const app = express();
 
 //Create Connection
-const conn = mysql.createConnection({
+const connPool = mysql.createPool({
+  connectionLimit: 100,
   host: 'localhost',
   user: 'root',
   password: 'root',
   database: 'drivers'
-});
-
-//connect to database
-conn.connect((err) => {
-  if (err) throw err;
-  console.log('Mysql Connected...');
 });
 
 //set views file
@@ -35,47 +30,75 @@ app.use('/assets', express.static(__dirname + '/public'));
 
 //route for homepage
 app.get('/', (req, res) => {
-  let sql = "SELECT * FROM driver";
-  let query = conn.query(sql, (err, results) => {
-    if (err) throw err;
-    res.render('driver_view', {
-      results: results
+  connPool.getConnection((err, conn) => {
+    if (err) {
+      conn.release();
+      return;
+    }
+    let sql = "SELECT * FROM driver";
+    let query = conn.query(sql, (err, results) => {
+      if (err) throw err;
+      res.render('driver_view', {
+        results: results
+      });
     });
+    conn.release();
   });
 });
 
 //route for insert data
 app.post('/save', (req, res) => {
-  let data = {
-    driver_name: req.body.driver_name,
-    driver_license: req.body.driver_license,
-    deprived_of_license: req.body.deprived_of_license
-  };
-  let sql = "INSERT INTO driver SET ?";
-  let query = conn.query(sql, data, (err, results) => {
-    if (err) throw err;
-    res.redirect('/');
+  connPool.getConnection((err, conn) => {
+    if (err) {
+      conn.release();
+      return;
+    }
+    let data = {
+      driver_name: req.body.driver_name,
+      driver_license: req.body.driver_license,
+      deprived_of_license: req.body.deprived_of_license
+    };
+    let sql = "INSERT INTO driver SET ?";
+    let query = conn.query(sql, data, (err, results) => {
+      if (err) throw err;
+      res.redirect('/');
+    });
+    conn.release();
   });
 });
 
 //route for update data
 app.post('/update', (req, res) => {
-  let sql = "UPDATE driver SET driver_name='" + req.body.driver_name
-    + "', driver_license='" + req.body.driver_license
-    + "', deprived_of_license='" + req.body.deprived_of_license
-    + "'  WHERE driver_id=" + req.body.id;
-  let query = conn.query(sql, (err, results) => {
-    if (err) throw err;
-    res.redirect('/');
+  connPool.getConnection((err, conn) => {
+    if (err) {
+      conn.release();
+      return;
+    }
+    let sql = "UPDATE driver SET driver_name='" + req.body.driver_name
+      + "', driver_license='" + req.body.driver_license
+      + "', deprived_of_license='" + req.body.deprived_of_license
+      + "'  WHERE driver_id=" + req.body.id;
+    let query = conn.query(sql, (err, results) => {
+      if (err) throw err;
+      res.redirect('/');
+    });
+    conn.release();
   });
 });
 
 //route for delete data
 app.post('/delete', (req, res) => {
-  let sql = "DELETE FROM driver WHERE driver_id=" + req.body.driver_id + "";
-  let query = conn.query(sql, (err, results) => {
-    if (err) throw err;
-    res.redirect('/');
+  connPool.getConnection((err, conn) => {
+    if (err) {
+      conn.release();
+      return;
+    }
+    let sql = "DELETE FROM driver WHERE driver_id=" + req.body.driver_id + "";
+    let query = conn.query(sql, (err, results) => {
+      if (err) throw err;
+      res.redirect('/');
+    });
+    conn.release();
   });
 });
 
